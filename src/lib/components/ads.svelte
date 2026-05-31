952dcb4a9c05a60030a04a76656364837ee58fe9
NamPE286 <nambuihung654@gmail.com>
2026-05-31 19:38:15 +0700

    upd

--- src/lib/components/ads.svelte
+++ src/lib/components/ads.svelte
@@ -7,8 +7,7 @@
 	export let unit = 'auto';
 	let adPushed = false;
 
-	// $: hidden = $user.checked ? $user.loggedIn && isActive($user.data.supporterUntil) : true;
-	$: hidden = true;
+	$: hidden = $user.checked ? $user.loggedIn && isActive($user.data.supporterUntil) : true;
 
 	function pushAd() {
 		if (hidden || adPushed) {
